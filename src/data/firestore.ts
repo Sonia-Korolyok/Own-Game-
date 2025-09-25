da8c1c381cbbec8f7ace4ec764e8b0f4e88d1a6f
seilah <princenoru@gmail.com>
2025-09-25 16:57:10 +0200

    hide-config: config data moved to a file

--- src/data/firestore.ts
+++ src/data/firestore.ts
@@ -1,16 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import firebaseConfig from "./firebaseConfig.ts";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyDGEpvdlVxn5rycWnPKRLhUpYNwHKBJWPs",
-    authDomain: "own-game-test.firebaseapp.com",
-    projectId: "own-game-test",
-    storageBucket: "own-game-test.firebasestorage.app",
-    messagingSenderId: "374395927592",
-    appId: "1:374395927592:web:be0b294be29e486b62891d",
-    measurementId: "G-Q34S6F2B6E"
-};
 
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
